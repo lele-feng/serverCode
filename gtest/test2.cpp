041d333cb01b4d7d203d0ed6558d9e7fa0be23e6
feng-lele <timing1109@gmail.com>
2020-05-22 21:48:11 +0800

    gtest

--- gtest/test2.cpp
+++ gtest/test2.cpp
@@ -12,12 +12,16 @@ public:
 class FooCalcTest:public testing::Test
 {
 protected:
+    static void SetUpTestCase(){std::cout<<"SetUpTestCase"<<std::endl;}
+    static void TearDownTestCase() {std::cout<<"TearDownTestCase"<<std::endl;}
     virtual void SetUp()
     {
+       std::cout<<"SetUP"<<std::endl;
         m_foo.Init();
     }
     virtual void TearDown()
     {
+       std::cout<<"TearDown"<<std::endl;
         m_foo.Finalize();
     }
 
@@ -25,6 +29,10 @@ protected:
 };
 
 TEST_F(FooCalcTest, testFooCalc_calc)
+{
+    EXPECT_EQ(12+16, m_foo.calc(12, 16));
+}
+TEST_F(FooCalcTest, testFooCalc_calc2)
 {
     EXPECT_EQ(12+16, m_foo.calc(12, 16));
 }
